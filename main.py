9ba25a109c3d3d5571823da724b8e78e495030ea
student06 <jakub.gola2@teb.edu.pl>
2025-05-16 12:49:15 +0200

    16.05.2025

---
+++ main.py
@@ -0,0 +1,22 @@
+class Sito:
+    def __init__(self, n):
+        self.n = n
+        self.pierwsze = [True for i in range(n+1)]
+
+    def generuj(self):
+        for i in range(2, int(self.n ** 0.5)+1):
+            if self.pierwsze[i]:
+                w = i*2;
+                while w <= self.n:
+                    self.pierwsze[w] = False
+                    w *= i
+
+    def wyswietl(self):
+        for i in range(2, self.n+1):
+            if self.pierwsze[i]:
+                print(i, end=' ')
+
+if __name__ == '__main__':
+    sito = Sito(100)
+    sito.generuj()
+    sito.wyswietl()
